e0636009f7e83f35bc4446cb9eb7cbfe7d47836c
namhyun99 <sshft@naver.com>
2023-03-04 11:08:30 +0900

    [ADD] Redis Cache Test & MyBatis Setting

---
+++ src/main/java/com/practice/springbatch/dto/Order.java
@@ -0,0 +1,22 @@
+package com.practice.springbatch.dto;
+
+import java.util.Date;
+
+import org.springframework.data.relational.core.mapping.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Order {
+  
+  private Integer orderNo;
+  private String productName;
+  private String userName;
+  private Integer price;
+  private Date createDate;
+
+}
---
+++ src/main/java/com/practice/springbatch/redis/RedisCacheService.java
@@ -0,0 +1,34 @@
+package com.practice.springbatch.redis;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.practice.springbatch.dao.OrderDao;
+import com.practice.springbatch.dto.Order;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Service
+public class RedisCacheService {
+  
+  @Autowired
+  private OrderDao orderDao;
+  
+  public Order createOrder(Order order) {
+    return orderDao.createOrder(order);
+  }
+  
+  public Order getAllOrder() {
+    return orderDao.findAll();
+  }
+  
+  public Order getOrder(Integer orderNo) {
+    long beforeTime = System.currentTimeMillis();
+    Order order = orderDao.findByOrderNo(orderNo);
+    long diffTime = System.currentTimeMillis() - beforeTime;
+    log.info("실행시간: [{}]ms ", diffTime);
+    return order;
+  }
+  
+}
